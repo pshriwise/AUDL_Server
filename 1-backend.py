3119ef0da7f00e477e173a7a512be0d700c80789
Pshriwise <shriwise@wisc.edu>
2014-02-22 12:46:02 -0600

    Created an attribute in Handler for storing top three assists data. Will load faster for a user like this if the data exists in the server beforehand.

--- 1-backend.py
+++ 1-backend.py
@@ -4,16 +4,16 @@ import SimpleHTTPServer, SocketServer
 from statget import top_three,name_to_id
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+    top_three_assists = top_three(name_to_id('Madison Radicals'), 'assists')
+
     def do_GET(self):
         self.send_response(200)     #  Send 200 OK
         self.send_header("Content-type","text")
         self.end_headers()
-        a = top_three(name_to_id('Madison Radicals'), 'goals')
-        self.end_headers()
-        self.wfile.write(a)
+        self.wfile.write(self.top_three_assists)
 
 
-PORT=1234
-httpd = SocketServer.ThreadingTCPServer(("", PORT), Handler) # Can also use ForkingTCPServer
+PORT=4000
+httpd = SocketServer.ThreadingTCPServer(("192.168.1.134", PORT), Handler) # Can also use ForkingTCPServer
 print "serving at port", PORT
 httpd.serve_forever()
