7cf8ba019c72327512a1257fc9a020a5131f360f
Pshriwise <shriwise@wisc.edu>
2014-02-22 14:42:18 -0600

    Implemented roster functions in backend.

--- backend.py
+++ backend.py
@@ -4,39 +4,31 @@ import SimpleHTTPServer, SocketServer
 from  statget import *
 top_three_assists = top_three(name_to_id('Madison Radicals'), 'assists')
 top_three_goals = top_three(name_to_id('Madison Radicals'), 'goals')
-
 team_rosters= rosters()
 
 def get_team_roster(team_id, rosters):
     for team, number, roster in team_rosters:
-        if (number == team_id):
-            return [team, number, roster]
-
-class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        if (str(number) == team_id):
+            return [team, roster]
 
 
-    def path_data(self):
-        paths=self.path.split('/')
-        paths.remove('')
-        if (paths[0] == 'teams') & (len(paths) == 1):
-            return team_dict()
-        elif paths[0] == 'teams':
-            get_team_roster(paths[1],team_rosters)
+def path_data(self):
+    paths=self.path.split('/')
+    paths.remove('')
+    if (paths[0] == 'teams') & (len(paths) == 1):
+        return team_dict()
+    elif paths[0] == 'teams':
+        return get_team_roster(paths[1],team_rosters)
                   
 
 
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path == "/goals":
             self.send_response(200)     #  Send 200 OK
             self.send_header("Content-type","text")
             self.end_headers()
-            self.wfile.write(top_three_goals)
-        elif self.path == "/assists":
-            self.send_response(200)     #  Send 200 OK
-            self.send_header("Content-type","text")
-            self.end_headers()
-            self.wfile.write(top_three_assists)
+            self.wfile.write(path_data(self))
 
 
 PORT=4000
