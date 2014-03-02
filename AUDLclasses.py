0fe0fca2603f33f9a7f266897312a4b46ac82872
Pshriwise <shriwise@wisc.edu>
2014-03-02 15:28:54 -0600

    Added a new file for server classes.
    Included the 'League' class and attributes in this file

---
+++ AUDLclasses.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+class League():
+    News = {};
+
+    Videos = {};
+
+    Teams = {};
+
+    This_week = [];
+
+    Apple_users = [];
+
+    Android_users = [];
+
+    Video_feeds = [];
+
+    RSS_feeds = [];
+
+    Top_fives = {};
+
+
