f0329a61128df11e1c63f81bad77098bd8c30c0f
Pshriwise <shriwise@wisc.edu>
2014-03-20 11:23:42 -0500

    Corrected port number for server.

--- server.py
+++ server.py
@@ -127,7 +127,7 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             self.wfile.write(json.dumps(path_data(self.path,AUDL)))
 
 
-PORT=4001
+PORT=4000
 httpd = SocketServer.ThreadingTCPServer(("192.168.1.134", PORT), Handler) # Can also use ForkingTCPServer
 print "serving at port", PORT
 httpd.serve_forever()
