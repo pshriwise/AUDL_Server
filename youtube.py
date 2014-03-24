7481d0194f23211a0d0cb5be349405bacf3dfe5b
cdrees19 <cdrees@wisc.edu>
2014-03-24 17:18:44 -0500

    spacing changes and comments

--- youtube.py
+++ youtube.py
@@ -3,14 +3,15 @@
 import urllib, json
 
 #want to return a string like this http://i.ytimg.com/vi/(link substring)/0.jpg
-def get_thumbnail(link):
+def get_thumbnail( link ):
     base = 'http://i.ytimg.com/vi/'
     end = '/0.jpg'
     link = link[31:-22]
     link = base + link + end
     return link
 
-def get_youtube():
+#returns a list of the videos on the AUDL Channel with each list having 1. title, 2. url, 3.jpeg
+def get_youtube( ):
     author = 'TheAUDLChannel'
 
     foundAll = False
@@ -19,16 +20,16 @@ def get_youtube():
     vids = []
     vidList = []
     while not foundAll:
-        inp = urllib.urlopen(r'http://gdata.youtube.com/feeds/api/videos?start-index={0}&max-results=50&alt=json&orderby=published&author={1}'.format( i, author ) )
+        inp = urllib.urlopen( r'http://gdata.youtube.com/feeds/api/videos?start-index={0}&max-results=50&alt=json&orderby=published&author={1}'.format( i, author ) )
         try:
             resp = json.load(inp)
             inp.close()
-            returnedVideos = resp['feed']['entry']
-            for video in returnedVideos:
+            retVids = resp['feed']['entry']
+            for video in retVids:
                 vids.append( video )
 
             i += 50
-            if ( len( returnedVideos ) < 50 ):
+            if ( len( retVids ) < 50 ):
                 foundAll = True
 
         except:
@@ -39,11 +40,10 @@ def get_youtube():
         #title = video['title']['$t']
         #link = video['link'][0]['href']
         #image = get_thumbnail(video['link'][0]['href'])
-        video_up = (video['title']['$t'], video['link'][0]['href'], get_thumbnail(video['link'][0]['href']))
-        vidList.append(video_up)
+        video_up = ( video['title']['$t'], video['link'][0]['href'], get_thumbnail( video['link'][0]['href'] ) )
+        vidList.append( video_up )
 
     return vidList
 
-#list = get_youtube()
-#print list
-
+list = get_youtube( )
+print list
