02aa470c1acb8bbc680535f616556ff4ed139fbe
Pshriwise <shriwise@wisc.edu>
2014-04-15 09:01:07 -0500

    Removed the corner case for the Seattle Raptors as they removed their Stat password.

--- AUDLclasses.py
+++ AUDLclasses.py
@@ -376,7 +376,7 @@ class Team():
         Assumes the ultimate-numbers info has already been loaded.
         """
         # These two teams currently have passwords, until we have access to their info, do nothing. 
-        if self.full_name() == "Seattle Raptors" or self.full_name() == "New York Empire" : return 0
+        if self.full_name() == "New York Empire" : return 0
         # get player summary data
         base_url = 'http://www.ultimate-numbers.com/rest/view'
         req1 = urllib2.Request(base_url+"/team/"+str(self.ID)+"/players/")
@@ -634,7 +634,7 @@ class Team():
     def get_games_info(self):
 
         # corner case for teams whose information requires authentication (for now)
-        if self.full_name() == "Seattle Raptors" or self.full_name() == "New York Empire": return 0
+        if self.full_name() == "New York Empire": return 0
         #get the list of games for the team from ultimate-numbers
         base_url = 'http://www.ultimate-numbers.com/rest/view'
 
