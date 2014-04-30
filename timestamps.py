3a5d373e88fbf8fe43f101f40b42846928b2c03a
Pshriwise <shriwise@wisc.edu>
2014-04-29 19:48:07 -0500

    Added a file for creating datetime objects for games with timezones included.

---
+++ timestamps.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python 
+
+from datetime import datetime as dt
+from datetime import timedelta
+from pytz import timezone
+import pytz 
+
+
+def game_ts(date,time):
+
+
+    tzs={'EST' : timezone('US/Eastern'),
+         'CST' : timezone('US/Central'),
+         'MST' : timezone('US/Mountain'),
+         'PST' : timezone('US/Pacific')
+        }
+
+    timestamp = date+" "+time
+
+    timestamp = timestamp.split()
+  
+    zone = timestamp[-1]
+
+    timestamp = dt.strptime(' '.join(timestamp[:-1]),"%m/%d/%y %I:%M %p")
+
+    timestamp = tzs[zone].localize(timestamp)
+
+    return timestamp
