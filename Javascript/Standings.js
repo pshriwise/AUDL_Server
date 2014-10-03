3978c6338d8af3bfaf4c4a7ddab36b08b3e46b8b
Ganeran <jibboo2000@gmail.com>
2014-10-02 22:33:42 -0500

    Standings with preload. Still no option for division.

--- Javascript/Standings.js
+++ Javascript/Standings.js
@@ -4,33 +4,37 @@ $(document).ready(function() {
 		jsonp: "callback",
 		dataType : "jsonp",
 		jsonpCallback : "getStandings",
-		cache : "true"
+		cache : true
 	});
 });
+var standingsTable = "";
 getStandings = function( data ) {
-		var parsedData = [];
-		var table = "";
-		parsedData=jQuery.parseJSON(data)
-		$.each( parsedData, function( divisionName, divisionArray ) {
-			if (table != "") table += "<br><br>";
-			table += "<table id=" + divisionName + " >";
-			table += "<tr>";
-			table += "<th colspan=2>" + divisionName + "</th>";
-			table += "<th>W</th>";
-			table += "<th>L</th>";
-			table += "<th>PD</th>";
-			table += "</tr>";
+	var parsedData = jQuery.parseJSON(data)
+	$.each( parsedData, function( divisionName, divisionArray ) {
+		if (standingsTable != "") standingsTable += "<br><br>";
+		standingsTable += "<table id=" + divisionName + " >";
+		standingsTable += "<tr>";
+		standingsTable += "<th colspan=2>" + divisionName + "</th>";
+		standingsTable += "<th>W</th>";
+		standingsTable += "<th>L</th>";
+		standingsTable += "<th>PD</th>";
+		standingsTable += "</tr>";
+		
+		$.each( divisionArray, function( j, teamArray ) {
+			var img = new Image();
+			img.src = "http://ec2-54-86-111-95.compute-1.amazonaws.com:4000/Logos/" + teamArray["id"] + ".png";
 			
-			$.each( divisionArray, function( j, teamArray ) {
-				table += "<tr>";
-				table += "<td><img src='http://ec2-54-86-111-95.compute-1.amazonaws.com:4000/Logos/" + teamArray["id"] + ".png'></td>";
-				table += "<td>" + teamArray["name"] + "</td>";
-				table += "<td>" + teamArray["wins"] + "</td>";
-				table += "<td>" + teamArray["losses"] + "</td>";
-				table += "<td>" + teamArray["plmn"] + "</td>";
-				table += "</tr>";
-			})
-			table += "</table>";
+			standingsTable += "<tr class='" + teamArray["name"].replace(/\s/g, '') + "'>";
+			standingsTable += "<td><img src='http://ec2-54-86-111-95.compute-1.amazonaws.com:4000/Logos/" + teamArray["id"] + ".png'></td>";
+			standingsTable += "<td>" + teamArray["name"] + "</td>";
+			standingsTable += "<td>" + teamArray["wins"] + "</td>";
+			standingsTable += "<td>" + teamArray["losses"] + "</td>";
+			standingsTable += "<td>" + teamArray["plmn"] + "</td>";
+			standingsTable += "</tr>";
 		})
-		$(".standings").append(table);
+		standingsTable += "</table>";
+	})
 };
+$( window ).load(function() { 
+	$(".standings").append(standingsTable);
+});
