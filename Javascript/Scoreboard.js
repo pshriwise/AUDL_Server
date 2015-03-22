9c96484bc1ca7f5518e783b831210ba0efc63b41
pshriwise <shriwise@wisc.edu>
2015-03-22 08:19:25 -0500

    Updated key value for the current score of game in the scoreboard widget.

--- Javascript/Scoreboard.js
+++ Javascript/Scoreboard.js
@@ -14,12 +14,12 @@ getScoreboard = function( data ) {
 	table += "<td><img src='http://ec2-54-86-111-95.compute-1.amazonaws.com:4001/Logos/TeamIcons_" + parsedData["hteam"] + ".png'></td>";
 	table += "<td>" + parsedData["hteam"]  + "</td>";
 	table += "<td>0</td><td>0</td><td>0</td><td>0</td>";
-	table += "<td>" + parsedData["hscore"]  + "</td>";
+	table += "<td>" + parsedData["home_score"]  + "</td>";
 	table += "</tr><tr>"
 	table += "<td><img src='http://ec2-54-86-111-95.compute-1.amazonaws.com:4001/Logos/TeamIcons_" + parsedData["ateam"] + ".png'></td>";
 	table += "<td>" + parsedData["ateam"]  + "</td>";
 	table += "<td>0</td><td>0</td><td>0</td><td>0</td>";
-	table += "<td>" + parsedData["ascore"]  + "</td>";
+	table += "<td>" + parsedData["away_score"]  + "</td>";
 	table += "</tr>";
 	table += "</table>";
 	$( window ).load(function() { 
