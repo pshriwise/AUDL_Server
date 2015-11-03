552d261ed82810d2917c938da8e35df935f92435
pshriwise <shriwise@wisc.edu>
2015-11-03 00:27:39 -0600

    Changed the script for adding games to dynamo to a simple one for establishing a connection to our aws account. (Keys obviously not included).

--- dynamo/dynamo_conn.py
+++ dynamo/dynamo_conn.py
@@ -1,25 +1,24 @@
 
 import urllib2,json
+import csv
 from boto.dynamodb2.layer1 import DynamoDBConnection
 from boto.dynamodb2.table import Table
+from boto.dynamodb2.items import Item
 from boto.dynamodb2.fields import HashKey
 from boto.dynamodb2.types import NUMBER
+import boto
+from os import environ
+
 
 base_url = 'http://www.ultianalytics.com/rest/view'
 req1 = urllib2.Request(base_url+"/team/"+'5671536392404992'+"/games/")
 response1 = urllib2.urlopen(req1,timeout=10)
 game_data = json.loads(response1.read(),parse_int=str,parse_constant=str)
 
-
-conn = DynamoDBConnection(host='localhost',aws_access_key_id='anything',aws_secret_access_key='anything',port=8000,is_secure=False)
-
-try:
-    conn.delete_table('games')
-except:
-    pass
-
-
-games = Table.create('games',schema=[HashKey('gameId')],throughput={'read':25,'write':25},connection=conn)
-
-for game in game_data:
-    games.put_item(data=game)
+key_dict={}
+with open('rootkey.csv','rb') as keyfile:
+    reader = csv.reader(keyfile,delimiter='=')
+    for row in reader:
+        key_dict[row[0]]=row[1]
+        
+conn = boto.dynamodb2.connect_to_region('us-east-1',aws_access_key_id=key_dict['AWSAccessKeyId'],aws_secret_access_key=key_dict['AWSSecretKey'])
