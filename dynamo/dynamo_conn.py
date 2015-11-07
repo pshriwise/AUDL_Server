d6b9992c84c1c9309fc53b39e0b0f0be49405e86
pshriwise <shriwise@wisc.edu>
2015-11-07 00:55:14 -0600

    Updated dynamo connection script.

--- dynamo/dynamo_conn.py
+++ dynamo/dynamo_conn.py
@@ -9,16 +9,11 @@ from boto.dynamodb2.types import NUMBER
 import boto
 from os import environ
 
-
-base_url = 'http://www.ultianalytics.com/rest/view'
-req1 = urllib2.Request(base_url+"/team/"+'5671536392404992'+"/games/")
-response1 = urllib2.urlopen(req1,timeout=10)
-game_data = json.loads(response1.read(),parse_int=str,parse_constant=str)
-
 key_dict={}
 with open('rootkey.csv','rb') as keyfile:
     reader = csv.reader(keyfile,delimiter='=')
     for row in reader:
         key_dict[row[0]]=row[1]
-        
-conn = boto.dynamodb2.connect_to_region('us-east-1',aws_access_key_id=key_dict['AWSAccessKeyId'],aws_secret_access_key=key_dict['AWSSecretKey'])
+
+def get_connection():
+    return boto.dynamodb2.connect_to_region('us-east-1',aws_access_key_id=key_dict['AWSAccessKeyId'],aws_secret_access_key=key_dict['AWSSecretKey'])
