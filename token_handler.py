2aebedbe1a1da21603284b8c6be9a1c5c6243a71
pshriwise <shriwise@wisc.edu>
2016-03-21 21:14:01 -0500

    Added some functions for creating a connection to aps and for sending a simple notification.

--- token_handler.py
+++ token_handler.py
@@ -1,5 +1,8 @@
 
+from apns import APNs, Frame, Payload
 
+
+token_hex = '0d4a8842d98d949225f1aeba1782604a8ae6fd9397c448c18ee52cf78933e368'
 
 def register_ios_token(path_entities):
     print("Registering ios token...")
@@ -8,3 +11,11 @@ def register_ios_token(path_entities):
 def register_android_token(path_entities):
     print("Registering Android token...")
     pass
+
+
+def get_apns_connection(cert_file = "AUDLCert.pem", key_file = "AUDL.pem"):
+    return APNs(use_sandbox=True, cert_file = "AUDLCert.pem", key_file="AUDLnopassword.pem")
+
+def send_notifications(message,token = token_hex):
+    conn = get_apns_connection()
+    conn.gateway_server.send_notification(token, Payload(alert = str(message), sound = 'default', badge = 1))
